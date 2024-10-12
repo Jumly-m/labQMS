a40a6cc77327dbb60aee73593c34ce29939fc73f
Jumly <maternjumly@gmail.com>
2024-10-12 17:20:47 +0300

    added vercel analytical

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import CourseCard from "./components/Card"
+import { Analytics } from "@vercel/analytics/react"
 
 
 
@@ -40,6 +41,7 @@ return (
       </div>
     </div>
     <Footer/>
+    <Analytics />
   
     </>
   )
