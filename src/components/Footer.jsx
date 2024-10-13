3666d6b61ba094273e6fa2e93638e5a1a1ba11b2
Jumly <maternjumly@gmail.com>
2024-10-13 11:21:33 +0300

    new card css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,18 @@
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; {new Date().getFullYear()} LabQMS</p>
+        <ul className="footer-links">
+          <li><a href="/services">Courses</a></li>
+          <li><a href="/contact">Articles</a></li>
+          <li><a href="/privacy">Presentations</a></li>
+          <li><a href="/privacy">Exams</a></li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
